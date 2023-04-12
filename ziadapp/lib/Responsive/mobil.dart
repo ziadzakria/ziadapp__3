2d1495263a2bcbeb2edcce208494e3e06888a0d0
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-04-12 19:03:40 +0200

    debugShow delet

--- ziadapp/lib/Responsive/mobil.dart
+++ ziadapp/lib/Responsive/mobil.dart
@@ -14,6 +14,7 @@ class _MobileScerrenState extends State<MobileScerren> {
       appBar: AppBar(
         title: Text("mobile screen"),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: []),
     );
   }
 }
