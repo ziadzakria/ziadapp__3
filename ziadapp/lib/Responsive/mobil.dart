cbd097a743005f927257b2ebc4c56d915f80b2a9
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-04-12 20:04:26 +0200

    page view

--- ziadapp/lib/Responsive/mobil.dart
+++ ziadapp/lib/Responsive/mobil.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../color.dart';
+import 'package:ziadapp/screens/home.dart';
+
+import '../screens/addpost.dart';
+import '../screens/profile.dart';
+import '../screens/search.dart';
 
 class MobileScerren extends StatefulWidget {
   const MobileScerren({Key? key}) : super(key: key);
@@ -11,37 +15,70 @@ class MobileScerren extends StatefulWidget {
 }
 
 class _MobileScerrenState extends State<MobileScerren> {
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("mobile screen"),
+        title: Text("AddPost Screen"),
       ),
       bottomNavigationBar: CupertinoTabBar(
-        backgroundColor: mobileBackgroundColor,
-        onTap: ,
-
-
-
-
-
-
-
-
-      )
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      (items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'home'),
-      ]),
+          backgroundColor: mobileBackgroundColor,
+          onTap: (index) {
+            _pageController.jumpToPage(index);
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  color: primaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.add_circle,
+                  color: secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite,
+                  color: secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  color: secondaryColor,
+                ),
+                label: ""),
+          ]),
+      body: PageView(
+        onPageChanged: (index) {
+          print("------- $index");
+        },
+        //physics: NeverScrollableScrollPhysics(),
+        //controller: _pageController,
+        children: [
+          Home(),
+          AddPost(),
+          Profile(),
+          Search(),
+        ],
+      ),
     );
   }
 }
---
+++ ziadapp/lib/screens/addpost.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddPost extends StatefulWidget {
+  const AddPost({Key? key}) : super(key: key);
+
+  @override
+  State<AddPost> createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AddPost Screen"),
+      ),
+    );
+  }
+}
