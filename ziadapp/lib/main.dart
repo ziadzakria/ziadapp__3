1a34f04c8e04d0614707f12532efb7736fdad5b7
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-04-12 19:06:47 +0200

    theme data=dart

--- ziadapp/lib/main.dart
+++ ziadapp/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home: Resposive(
         myMobileScreen: MobileScerren(),
         myWebScreen: WebScerren(),
