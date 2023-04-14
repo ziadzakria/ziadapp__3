778ea2fb1568c3e83a16d10b07f56e3a2e50b175
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-04-14 16:59:48 +0200

    widthScreen >600 app bar to web

--- ziadapp/lib/screens/home.dart
+++ ziadapp/lib/screens/home.dart
@@ -16,26 +16,28 @@ class _HomeState extends State<Home> {
 
     return Scaffold(
       backgroundColor: mobileBackgroundColor,
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.messenger_outline,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.logout,
-              )),
-        ],
-        backgroundColor: mobileBackgroundColor,
-        title: SvgPicture.asset(
-          "ziadapp/asstes/img/download.jpg",
-          color: primaryColor,
-          height: 32,
-        ),
-      ),
+      appBar: widthScreen > 600
+          ? null
+          : AppBar(
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.messenger_outline,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.logout,
+                    )),
+              ],
+              backgroundColor: mobileBackgroundColor,
+              title: SvgPicture.asset(
+                "ziadapp/asstes/img/download.jpg",
+                color: primaryColor,
+                height: 32,
+              ),
+            ),
       body: Column(
         children: [
           Padding(
