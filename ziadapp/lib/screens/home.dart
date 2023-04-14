154b23df9f1daa74e9d7800e85cf57e65ff541cf
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-04-14 14:38:25 +0200

    add text

--- ziadapp/lib/screens/home.dart
+++ ziadapp/lib/screens/home.dart
@@ -48,6 +48,10 @@ class _HomeState extends State<Home> {
                 SizedBox(
                   width: 17,
                 ),
+                Text(
+                  "ziad elbana",
+                  style: TextStyle(fontSize: 15),
+                ),
               ],
             ),
           ],
