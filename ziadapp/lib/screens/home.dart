358e92d75c26cabef9bea8eff37c6a85150d3a27
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-04-14 14:21:40 +0200

    CircleAvatar to image

--- ziadapp/lib/screens/home.dart
+++ ziadapp/lib/screens/home.dart
@@ -15,27 +15,33 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: mobileBackgroundColor,
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.messenger_outline,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.logout,
-              )),
-        ],
         backgroundColor: mobileBackgroundColor,
-        title: SvgPicture.asset(
-          "ziadapp/asstes/img/download.jpg",
-          color: primaryColor,
-          height: 32,
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.messenger_outline,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.logout,
+                )),
+          ],
+          backgroundColor: mobileBackgroundColor,
+          title: SvgPicture.asset(
+            "ziadapp/asstes/img/download.jpg",
+            color: primaryColor,
+            height: 32,
+          ),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            Row(
+              children: [CircleAvatar()],
+            ),
+          ],
+        ));
   }
 }
