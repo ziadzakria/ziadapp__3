3205a48899ebd10506d2ce26aa19980349d69afd
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-04-16 15:37:52 +0200

    navigateScreen

--- ziadapp/lib/Responsive/web.dart
+++ ziadapp/lib/Responsive/web.dart
@@ -4,7 +4,7 @@ import 'package:ziadapp/screens/home.dart';
 import '../screens/search.dart';
 import '../screens/addpost.dart';
 import '../screens/profile.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WebScerren extends StatefulWidget {
   const WebScerren({Key? key}) : super(key: key);
@@ -14,81 +14,82 @@ class WebScerren extends StatefulWidget {
 }
 
 class _WebScerrenState extends State<WebScerren> {
-    final PageController _pageController = PageController();
+  final PageController _pageController = PageController();
 
-
- navigate2Screen(int indexx) {
+  navigateScreen(int indexx) {
     _pageController.jumpToPage(indexx);
-    setState(() {
-      page = indexx;
-    });
+    setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.home,
-              color: primaryColor,
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.home,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(0);
+              },
             ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: secondaryColor,
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(1);
+              },
             ),
-            onPressed: () {},
-            _pageController.jumpToPage(1);
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.add_a_photo,
-              color: secondaryColor,
+            IconButton(
+              icon: Icon(
+                Icons.add_a_photo,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(2);
+              },
             ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.favorite,
-              color: secondaryColor,
+            IconButton(
+              icon: Icon(
+                Icons.favorite,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(3);
+              },
             ),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.person,
-              color: secondaryColor,
+            IconButton(
+              icon: Icon(
+                Icons.person,
+                color: secondaryColor,
+              ),
+              onPressed: () {
+                navigateScreen(4);
+              },
             ),
-            onPressed: () {},
-          ),
-           backgroundColor: mobileBackgroundColor,
-        title: SvgPicture.asset(
-          "ziadapp\asstes\img\download.jpg",
-          color: primaryColor,
-          height: 32,
-        ),
-      ),
-        body: PageView(
-           controller: _pageController,
-        onPageChanged: (index) {
-         
-        },
-        physics: NeverScrollableScrollPhysics(),
-        children: [
-          Home(),
-          Search(),
-                    AddPost(),
-
-                     Profile(),
-
-
-
-         
-        ],
-      ),
+          ],
+          backgroundColor: mobileBackgroundColor,
+          title: SvgPicture.asset(
+            "ziadapp/asstes/img/ownload.jpg",
+            color: primaryColor,
+            height: 32,
+          )),
+    );
+    body:
+    PageView(
+      controller: _pageController,
+      onPageChanged: (index) {},
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Home(),
+        Search(),
+        AddPost(),
+        Profile(),
+      ],
     );
   }
 }
