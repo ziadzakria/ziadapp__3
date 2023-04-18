b59cda69b1541235f68efb582cb21112aa9698c2
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-04-18 20:13:57 +0200

    add backgroundColor

--- ziadapp/lib/screens/search.dart
+++ ziadapp/lib/screens/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../color.dart';
 
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
@@ -12,6 +13,7 @@ class _SearchState extends State<Search> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
         title: TextFormField(
           decoration: const InputDecoration(labelText: 'Search for a user...'),
         ),
